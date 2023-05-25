52da6a60a5b7f68dada8253f89a1fb99cb45e9a8
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-05-25 15:42:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/YourPet">
+      <BrowserRouter basename="/react_node_bc_44_smokekans">
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <App />
